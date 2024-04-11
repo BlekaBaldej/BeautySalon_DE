9f0d66475d3778f1b9a0787fd37620b1b88a786b
sppod <sppod@DESKTOP-QN4NBJG>
2024-04-11 22:54:39 +0300

    Добавьте файлы проекта.

---
+++ BeautySalon_DE/Core/Tools.cs
@@ -0,0 +1,13 @@
+﻿using BeautySalon_DE.ModelEF;
+
+namespace BeautySalon_DE
+{
+    public delegate void UpdateProducts();
+    public delegate void UpdateCostProducts(Product product);
+    public static class Getter
+    {
+        public static Model1 db = new Model1();
+        public static UpdateProducts updateProducts = Program.mainForm.PrepareData;
+        public static UpdateCostProducts updateCostProducts = Program.mainForm.UpdateProductCostList;
+    }
+}
